658cb55fd35cc152369b75e8707ce9ae4b344ad3
Aviral2410 <aviral@example.com>
2026-04-19 12:24:53 +0530

    fix

--- services/sms-common/src/main/java/com/sms/common/logging/LoggingAspect.java
+++ services/sms-common/src/main/java/com/sms/common/logging/LoggingAspect.java
@@ -14,7 +14,7 @@ import java.util.Arrays;
 @Slf4j
 public class LoggingAspect {
 
-    @Around("execution(* com.sms..*(..)) && !within(com.sms.common.logging..*)")
+    @Around("execution(* com.sms..*(..)) && !within(com.sms.common.logging..*) && !within(jakarta.servlet.Filter+)")
     public Object profileAllMethods(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
         String className = methodSignature.getDeclaringType().getSimpleName();
